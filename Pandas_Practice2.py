aaaa3340b14ae0d05bd859837de74e749e09d579
vsuravaram <vr.0451@gmail.com>
2022-12-06 17:01:06 -0600

    update

--- Pandas_Practice2.py
+++ Pandas_Practice2.py
@@ -54,4 +54,92 @@ s=pd.Series([1,2,3,4], index=['a','g','r','p'])
 print(s['a'])# """ OP: 1 """
 #retrieve multiple elements
 print (s[['a','r','p']])
-print(s['f'])  # """ KeyError: 'f' """
+#print(s['f'])  # """ KeyError: 'f' """
+
+#####################################################  DataFrame
+####
+###
+#
+##
+import pandas as pd
+df = pd.DataFrame()
+print(df)
+"""OP:
+Empty DataFrame
+Columns: []
+Index: []"""
+
+data=[1,2,3,4,5] # from list
+df=pd.DataFrame(data)
+print(df)
+"""OP:
+   0
+0  1
+1  2
+2  3
+3  4
+4  5"""
+
+data=[['venkat',30],['Bob',45]]
+df=pd.DataFrame(data,columns=['Name', 'Age'], dtype=float)
+print(df)
+"""OP:
+     Name  Age
+0  venkat   30
+1     Bob   45"""
+
+import pandas as pd
+data={'name':['ven','ben','rem'], 'age':[34,45,32]}
+df=pd.DataFrame(data)
+print(df)
+"""OP:
+  name  age
+0  ven   34
+1  ben   45
+2  rem   32"""
+
+import pandas as pd
+data = {'Name':['Tom', 'Jack', 'Steve', 'Ricky'],'Age':[28,34,29,42]}
+df = pd.DataFrame(data, index=['rank1','rank2','rank3','rank4'])  ## Indexed dataframe
+print (df)
+"""OP:
+        Name  Age
+rank1    Tom   28
+rank2   Jack   34
+rank3  Steve   29
+rank4  Ricky   42
+"""
+###   Create a DataFrame from Dict of Series
+import pandas as pd
+
+d = {'one' : pd.Series([1, 2, 3], index=['a', 'b', 'c']),
+   'two' : pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])}
+
+df = pd.DataFrame(d)
+print (df)
+print(df['one'])
+print("adding new column called three")
+df['three']=pd.Series([10,20,30],index=['a','b','d'])
+print (df)
+"""op:
+   one  two
+a  1.0    1
+b  2.0    2
+c  3.0    3
+d  NaN    4"""
+
+"""OP:
+a    1.0
+b    2.0
+c    3.0
+d    NaN
+Name: one, dtype: float64"""
+
+"""OP:
+   one  two  three
+a  1.0    1   10.0
+b  2.0    2   20.0
+c  3.0    3    NaN
+d  NaN    4   30.0"""
+
+
